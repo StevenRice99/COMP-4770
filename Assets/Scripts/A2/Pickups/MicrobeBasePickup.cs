818cbef25fc7e7ccecf1c89b759226d74e82d060
Steven Rice <TheStevenRice@Gmail.com>
2022-02-24 19:44:28 -0500

    pickups documentation

--- Assets/Scripts/A2/Pickups/MicrobeBasePickup.cs
+++ Assets/Scripts/A2/Pickups/MicrobeBasePickup.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 
 namespace A2.Pickups
 {
+    /// <summary>
+    /// Base class for pickups for microbes.
+    /// </summary>
     public abstract class MicrobeBasePickup : MonoBehaviour
     {
+        /// <summary>
+        /// The behaviour of the pickup.
+        /// </summary>
+        /// <param name="microbe">The microbe which picked up this pickup.</param>
         protected abstract void Execute(Microbe microbe);
 
         private void Update()
         {
+            // Get all microbes near to this pickup.
             Microbe[] microbes = AgentManager.Singleton.Agents.Where(a => Vector3.Distance(a.transform.position, transform.position) <= MicrobeManager.MicrobeManagerSingleton.MicrobeInteractRadius).Cast<Microbe>().ToArray();
             if (microbes.Length == 0)
             {
                 return;
             }
             
+            // Activate this pickup for the nearest microbe.
             Microbe microbe = microbes.OrderBy(m => Vector3.Distance(m.transform.position, transform.position)).First();
             microbe.AddMessage("Collecting pickup.");
             microbe.PlayPickupAudio();
