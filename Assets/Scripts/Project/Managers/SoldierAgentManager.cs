907300b90105deb024a3c2e34c6592f3c1052dd3
Steven Rice <TheStevenRice@Gmail.com>
2022-04-15 22:59:28 -0400

    start of logic for AI

---
+++ Assets/Scripts/Project/Managers/SoldierAgentManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Project.Managers
+{
+    public class SoldierAgentManager : AgentManager
+    {
+        /// <summary>
+        /// Getter to cast the AgentManager singleton into a SoldierManager.
+        /// </summary>
+        public static SoldierAgentManager SoldierAgentManagerSingleton => Singleton as SoldierAgentManager;
+
+        [Min(1)]
+        public int health = 100;
+
+        [Min(0)]
+        public float respawn = 10;
+    }
+}
---
+++ Assets/Scripts/Project/Minds/SoldierBrain.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Project.Managers;
+using UnityEngine;
+
+namespace Project.Minds
+{
+    public class SoldierBrain : Mind
+    {
+        private static readonly List<SoldierBrain> TeamRed = new();
+        
+        private static readonly List<SoldierBrain> TeamBlue = new();
+
+        private enum SoliderRole : byte
+        {
+            Dead,
+            Deciding,
+            Collector,
+            Attacker,
+            Defender
+        }
+        
+        private SoliderRole _role;
+
+        private bool _redTeam;
+
+        private int _health;
+
+        private bool Alive => _role != SoliderRole.Dead;
+        
+        public override Action[] Think()
+        {
+            return null;
+        }
+
+        public void Damage(int amount)
+        {
+            if (_role == SoliderRole.Dead)
+            {
+                return;
+            }
+            
+            _health -= amount;
+            if (_health > 0)
+            {
+                return;
+            }
+
+            StopAllCoroutines();
+            StartCoroutine(Respawn());
+        }
+
+        public void Heal(int amount)
+        {
+            if (_role == SoliderRole.Dead)
+            {
+                return;
+            }
+
+            _health += amount;
+            if (_health > SoldierAgentManager.SoldierAgentManagerSingleton.health)
+            {
+                _health = SoldierAgentManager.SoldierAgentManagerSingleton.health;
+            }
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+            
+            Agent.Wander = true;
+            
+            _redTeam = TeamRed.Count <= TeamBlue.Count;
+            if (_redTeam)
+            {
+                TeamRed.Add(this);
+            }
+            else
+            {
+                TeamBlue.Add(this);
+            }
+            
+            // MOVE TO SPAWN POSITION.
+            _role = GetRole();
+            _health = SoldierAgentManager.SoldierAgentManagerSingleton.health;
+        }
+
+        private SoliderRole GetRole()
+        {
+            bool collector = false;
+            int attackDefend = 0;
+            foreach (SoldierBrain brain in GetTeam())
+            {
+                switch (brain._role)
+                {
+                    case SoliderRole.Collector:
+                        collector = true;
+                        continue;
+                    case SoliderRole.Attacker:
+                        attackDefend++;
+                        continue;
+                    default:
+                        attackDefend--;
+                        break;
+                }
+            }
+
+            return !collector ? SoliderRole.Collector : attackDefend >= 0 ? SoliderRole.Defender : SoliderRole.Attacker;
+        }
+
+        private IEnumerator Respawn()
+        {
+            _role = SoliderRole.Dead;
+            
+            foreach (SoldierBrain brain in GetTeam())
+            {
+                brain._role = SoliderRole.Deciding;
+            }
+            
+            foreach (SoldierBrain brain in GetTeam())
+            {
+                brain.GetRole();
+            }
+            
+            yield return new WaitForSeconds(SoldierAgentManager.SoldierAgentManagerSingleton.respawn);
+            
+            // MOVE TO SPAWN POSITION.
+            _health = SoldierAgentManager.SoldierAgentManagerSingleton.health;
+            _role = GetRole();
+        }
+
+        private IEnumerable<SoldierBrain> GetTeam()
+        {
+            // ORDER BY DISTANCE TO ENEMY FLAG.
+            return (_redTeam ? TeamRed : TeamBlue).Where(s => s.Alive);
+        }
+    }
+}
